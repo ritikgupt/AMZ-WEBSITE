e938b76d8488db97da778a25dcad9b575c4170de
ritikgupt <ritikgupta89369@gmail.com>
2020-06-12 17:52:56 +0530

    database connected

--- routes/home.js
+++ routes/home.js
@@ -1,14 +1,16 @@
 var express = require('express');
 var router = express.Router();
+const db = require('../db/db');
 
 router.get('/',async(req,res)=>{
+    console.log(req.body.img_url)
     try{
+        let a = await db.add_slider(req)
         res.render('home');
-    }catch{
-        res.json({"message":error})
+    }catch(e){
+        res.json({"message":e})
     }
 })
-
 
 
 
