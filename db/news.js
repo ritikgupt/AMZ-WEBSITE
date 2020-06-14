5ac2de3cb144cee79f06064c69842e521d6d63a2
ritikgupt <ritikgupta89369@gmail.com>
2020-06-14 18:43:48 +0530

    news route created

---
+++ db/news.js
@@ -0,0 +1,66 @@
+const mysql = require('mysql')
+const pool = mysql.createPool({
+    password:'Ritikgupta10$',
+    user:'root',
+    database:'AMZ_WEBSITE',
+    host:'localhost',
+    port:'3306'
+});
+
+let news={};
+
+news.add = (req) =>{
+    
+    return new Promise((resolve, reject) => {
+        pool.query('insert into news (img_url,description,heading,date) values (?,?,?,CURDATE())', 
+        [req.img_url,req.description,req.heading], (err,results) =>{
+            if(err)
+            return reject(err);
+            return resolve(results);
+        });
+    });
+}
+news.deleteAll = () =>{
+    
+    return new Promise((resolve, reject) => {
+        pool.query('delete from news', (err,results) =>{
+            if(err)
+            return reject(err);
+            return resolve(results);
+        });
+    });
+}
+news.deleteOne = (req) =>{
+    
+    return new Promise((resolve, reject) => {
+        pool.query('Delete from news where (newsid)=(?)', 
+        [req.id], (err,results) =>{
+            if(err)
+            return reject(err);
+            return resolve(results);
+        });
+    });
+}
+news.edit = (req,request) =>{
+    
+    return new Promise((resolve, reject) => {
+        pool.query('update news set img_url=?,description=?,heading=? where newsid=?', 
+        [req.img_url,req.description,req.heading,request.id]
+        , (err,results) =>{
+            if(err)
+            return reject(err);
+            return resolve(results);
+        });
+    });
+}
+news.show = () =>{
+    return new Promise((resolve, reject) => {
+        pool.query('Select * from news', 
+         (err,results) =>{
+            if(err)
+            return reject(err);
+            return resolve(results);
+        });
+    });
+}
+module.exports = news
--- routes/home.js
+++ routes/home.js
@@ -1,10 +1,13 @@
-var express = require('express');
-var router = express.Router();
-const slider = require('../db/image');
+var express = require('express')
+var router = express.Router()
+const slider = require('../db/image')
+const news = require('../db/news')
 
 router.get('/',async(req,res)=>{
-    let a = await slider.show()
+    let a = await slider.show('home_slider')
+    let b = await news.show('news')
     try{
+        console.log(b)
         console.log(a)
         res.render('home');
     }catch(e){
